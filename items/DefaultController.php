7dfe7a0b00fc1188986e4618a02f44a4fccdebe4
cosmin <cosmin.beleuz@evolution.ro>
2015-03-25 14:13:48 +0200

    Revert "all controller now extend from DefaultController, changed layout, and busted whitespace"
    This reverts commit 1c06bbc2d5da6627077cff05ed8e553a3135c8b3.

--- items/DefaultController.php
+++ items/DefaultController.php
@@ -4,12 +4,9 @@ namespace mdm\admin\items;
 
 class DefaultController extends \yii\web\Controller
 {
-
-	public $layout = 'top-menu';
-
-	public function actionIndex()
-	{
-		return $this->render('index');
-	}
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
 
 }
